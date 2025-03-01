e0ad2877fbfe4fa7700b5c64c4420fb054a5e69f
mustafaAras98 <mustafa_aras_25@hotmail.com>
2025-03-01 16:29:59 +0300

    Update App.jsx
    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,57 @@
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import "./App.css";
 import questionBankLogo from "/QuestionbankLogo.png";
 
+const redirectArray = ["Album", "Image"];
 function App() {
-  const hasRedirected = useRef(false);
-  const path = window.location.pathname;
-
   function getMobileOS() {
-    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
-    if (/android/i.test(userAgent)) return "Android";
-    return "unknown";
-  }
+    const userAgent = navigator.userAgent || window.opera;
+    if (/windows phone/i.test(userAgent)) {
+      return "Windows Phone";
+    }
 
-  function redirectUser() {
-    if (hasRedirected.current) return;
-    hasRedirected.current = true;
-    const deepLink = `https://questionbankwebhost.firebaseapp.com${path}`;
-    const playStore =
-      "https://play.google.com/store/apps/details?id=com.questionbank";
+    if (/android/i.test(userAgent)) {
+      return "Android";
+    }
 
-    const os = getMobileOS();
-    if (os === "Android") {
-      window.location.href = deepLink;
-      setTimeout(() => {
-        window.location.href = playStore;
-      }, 1500);
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+      return "iOS";
     }
   }
 
   useEffect(() => {
+    function redirectUser() {
+      const path = window.location.pathname;
+      const pathSegments = path.split("/").filter(Boolean);
+
+      if (
+        redirectArray.includes(pathSegments[0]) &&
+        pathSegments.length === 2
+      ) {
+        const playStore =
+          "https://play.google.com/store/apps/details?id=com.questionbank";
+        const os = getMobileOS();
+        if (os === "Android") {
+          setTimeout(() => {
+            window.location.href = playStore;
+          }, 1500);
+        }
+      }
+    }
     redirectUser();
   }, []);
 
   return (
     <>
       <div>
-          <img
-            src={questionBankLogo}
-            className="logo"
-            alt="QuestionBank logo"
-          />
+        <img src={questionBankLogo} className="logo" alt="QuestionBank logo" />
       </div>
       <h1>QuestionBank</h1>
       <p className="info">
-        This open-source React Native app empowers students to overcome academic
-        challenges by providing a collaborative learning platform. Users can
-        easily snap photos of incorrect exam questions and share them with their
-        study groups
+        This open-source React Native app helps students tackle academic
+        challenges through a collaborative learning platform. Users can quickly
+        capture photos of incorrect exam questions and share them with their
+        study groups for better understanding.
       </p>
       <button
         onClick={() => {
@@ -54,7 +59,7 @@ function App() {
             "https://play.google.com/store/apps/details?id=com.questionbank";
         }}
       >
-        Download QuestionBank on Appstore
+        Download QuestionBank on Play Store
       </button>
     </>
   );
